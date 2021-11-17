724eebb73439fe171ea852d4e3052eea97d3b446
Kevinmen7 <kevingfx71@gmail.com>
2021-11-17 10:48:47 -0600

    Subiendo parte de Jasser

---
+++ Domain/Calendario.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Enums;
+using System;
+
+namespace Domain
+{
+    public class Calendario
+    {
+        public int Id { get; set; }
+        public Tipo Tipo { get; set; }
+        public Estado Estado { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public DateTime FechaPaga { get; set; }
+        public decimal Principal { get; set; }
+        public decimal Interest { get; set; }
+        public decimal Impuestos { get; set; }
+        public decimal MontoTotal { get; set; }
+
+    }
+}
